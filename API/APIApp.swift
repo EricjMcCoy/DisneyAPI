b6244d01b4b6a059c63e9d3966804a25a675304d
Eric McCoy <divereric0@gmail.com>
2024-12-16 14:57:07 -0500

    Initial Commit

---
+++ API/APIApp.swift
@@ -0,0 +1,17 @@
+//
+//  APIApp.swift
+//  API
+//
+//  Created by Eric McCoy (student LM) on 12/16/24.
+//
+
+import SwiftUI
+
+@main
+struct APIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
