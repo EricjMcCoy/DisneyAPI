3eb4fe3fc19240b27eeda75d59774c05dddf4c53
Roy-Da <Royda08@outlook.com>
2024-12-18 12:51:11 -0500

    added view

---
+++ API/Views/InfoView.swift
@@ -0,0 +1,18 @@
+//
+//  InfoView.swift
+//  API
+//
+//  Created by Roy David (student LM) on 12/18/24.
+//
+
+import SwiftUI
+
+struct InfoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InfoView()
+}
